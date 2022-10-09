eeb78448b9d054de9060be159556502886d2429c
Rossy Perez <rossyesmeral@gmail.com>
2022-10-08 23:38:25 -0300

    Chequear y verificar las urls

--- processLib.js
+++ processLib.js
@@ -33,3 +33,47 @@ const readDirectory = (path) => {
         return false;
     }
 }
+
+/**
+ * Funcion para extraer las urls de los archivos .md
+ * @param files
+ * @returns {*[]}
+ */
+const checkUrls = (files) => {
+  const fileDescription = [];
+  // Esto es una expresion regular para obtener todas las URLs
+  const getUrlMd = /(((https?:\/\/)|(http?:\/\/)|(www\.))[^\s\n]+)(?=\))/g
+  files.forEach(file => {
+    const fileMd = fs.readFileSync(file, {encoding: "utf8", flag: "r"});
+    const getAllUrls = fileMd.match(getUrlMd);
+    fileDescription.push({
+      name: file,
+      urls: getAllUrls,
+      quantity: getAllUrls.length
+    })
+  })
+  return fileDescription;
+}
+
+/**
+ * Función para validar las URLs
+ * @param dataMd
+ * @returns {Promise<Awaited<unknown>[]>}
+ */
+const verifyUrl = (dataMd) => {
+  console.log('\n')
+  console.log('============ FILES URLS ============\n'.blue)
+  const info = dataMd.map((data) => {
+    return data.urls.map((url) => {
+      return fetch(url).then((res) => {
+        return {
+          file: data.name,
+          href: url,
+          status: res.status,
+          ok: res.ok ? 'ok' : 'fail'
+        }
+      })
+    })
+  })
+  return Promise.all(info.flat());
+}
